bd134f847f9e152e9217f49efadb54a88fb78d63
Alexandr Sergeev <ntl1221@yandex.ru>
2024-09-03 01:16:00 +0300

    рефакторинг

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,13 +1,39 @@
 import arrow from 'src/images/arrow.svg';
 import clsx from 'clsx';
 import styles from './ArrowButton.module.scss';
+import React, { useState } from 'react';XMLDocument
 
 type ArrowButtonProps = {
-  onClick: () => void; // Функция, которая вызывается при клике
+  onClick: () => void;
 	isOpen: boolean;
 };
-																												//получение через пропс для нажатия и изменения внешного вида.
+
+//получение через пропс для нажатия и изменения внешного вида.
 export const ArrowButton: React.FC<ArrowButtonProps> = ({ onClick, isOpen }) => {
+
+	//обновления состояния подсветки
+	const [isHovered, setIsHovered] = useState(false);
+
+	const handleArrowButtonEnter = () => {
+    setIsHovered(true);
+  };
+
+	const handleArrowButtonLeave = () => {
+    setIsHovered(false);
+  };
+
+	//обновления состояния подсветки
+	const [isPressed, setIsPressed] = useState(false);
+
+	const handleMouseDown = () => {
+    setIsPressed(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsPressed(false);
+  };
+
+	const backgroundColor = isPressed ? 'grey' : isHovered ? '#ffd700' : 'black';
 
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
@@ -15,8 +41,13 @@ export const ArrowButton: React.FC<ArrowButtonProps> = ({ onClick, isOpen }) => 
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			onClick={onClick} 
-			className={clsx(styles.container, isOpen && styles.container_open)}>
+			onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+			onClick={onClick}
+			className={clsx(styles.container, isOpen && styles.container_open)}
+			style={{ backgroundColor }} 
+			onMouseEnter={handleArrowButtonEnter}
+			onMouseLeave={handleArrowButtonLeave}>
 			<img src={arrow} alt='иконка стрелочки' className={clsx(styles.arrow, isOpen && styles.arrow_open)} />
 		</div>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,27 +1,82 @@
+import { useState, useEffect, SyntheticEvent } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-import { Text } from 'components/text'
+import { Text } from 'components/text';
+import { Select } from 'components/select/Select'
+import { RadioGroup } from 'components/radio-group';
+import { defaultArticleState, fontSizeOptions, AppState, backgroundColors, fontFamilyOptions, fontColors, contentWidthArr } from 'src/constants/articleProps';
+import { Separator } from 'components/separator'
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import storybookStyles from '../story-decorator/StoryDecorator.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsProps =  {
+	title: string,
+	setAppState: any
+	appState: AppState
+}
 
-	//состояние открытия.закрытия
+export const ArticleParamsForm = ({ setAppState, appState, title }: ArticleParamsProps) => {
+
+	//создание начального состояния открытия.закрытия окна
 	const [isOpen, setIsOpen] = useState(false);
 
-	//функция переклюяения состояния
+	//Переключения состояния открытия.закрытия
 	const OpenParamsForm = () => {
 		setIsOpen(prevState => !prevState);
+		
 	}
+
+	//кнопка выбора и передача значения в пропс
+	const submitClick = (e: SyntheticEvent) => {
+		setAppState(optionSelected);
+		e.preventDefault();
+	};
+
+	//кнопка сброса
+	const resetClick = () => {
+		setAppState(defaultArticleState);
+	}
+
+	//создание начального состояния выбора опции
+	const [optionSelected, setOptionSelected] = useState(appState);
+
 	return (
 		<>
-		  {/* Передаем функцию и состояние при каждом рендере в кнопку */}
+		  {/* Передаем состояние при каждом рендере в кнопку */}	
 			<ArrowButton onClick={OpenParamsForm} isOpen={isOpen}/> 
 			<aside className={clsx(styles.container, isOpen && styles.container_open)}>
-				<form className={styles.form}>
-				<Text as='h1' size={31} weight={800} uppercase dynamicLite>Задайте параметры</Text>
+				<form className={styles.form} onSubmit={submitClick} onReset={resetClick}>
+						
+						<Text as='h1' size={31} weight={800} uppercase>{title}</Text>
+						<div className={storybookStyles.storybookContainer}></div>
+						<Select title={'Шрифт'}
+										options={fontFamilyOptions}
+										selected={optionSelected.fontFamilyOption}
+										onChange={(fontFamilyOption) => setOptionSelected({ ...optionSelected, fontFamilyOption })} />
+						<div className={storybookStyles.storybookContainer}></div>
+						<RadioGroup title={'Размер шрифта'}
+										options={fontSizeOptions} name={'fontSize'}
+										selected={optionSelected.fontSizeOption}
+										onChange={(fontSizeOption) => setOptionSelected({ ...optionSelected, fontSizeOption })} />
+						<div className={storybookStyles.storybookContainer}></div>
+						<Select title={'Цвет шрифта'}
+										options={fontColors}
+										selected={optionSelected.fontColor}
+										onChange={(fontColor) => setOptionSelected({ ...optionSelected, fontColor })} />
+						<div className={storybookStyles.storybookContainer}></div>
+						<Separator/>
+						<div className={storybookStyles.storybookContainer}></div>
+						<Select title={'Цвет фона'}
+										options={backgroundColors}
+										selected={optionSelected.backgroundColor}
+										onChange={(backgroundColor) => setOptionSelected({ ...optionSelected, backgroundColor })} />
+						<div className={storybookStyles.storybookContainer}></div>
+						<Select title={'Ширина контента'}
+										options={contentWidthArr}
+										selected={optionSelected.contentWidth}
+										onChange={(contentWidth) => setOptionSelected({ ...optionSelected, contentWidth })} />
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
